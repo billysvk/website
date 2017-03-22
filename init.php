e16383fe4a9823e704227eace495cfddad9c8557
Billy Savakis <billysvk@gmail.com>
2017-03-22 11:50:27 +0200

    Update init.php

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 	header('Content-type: text/html; charset=utf-8'); // Το χρησιμοποιούμε για να φαίνονται σωστά τα ελληνικά
 	session_start ();
 
-	$con = mysql_connect ( "localhost", "my_usr", "my_password" ); // Σύνδεση με τη βάση
+	$con = mysql_connect ( "localhost", "root", "" ); // Σύνδεση με τη βάση
 	if ( !$con )
 	{
 		die ( "Δεν ήταν δυνατή η σύνδεση με τη βάση δεδομένων: ".mysql_error () );
