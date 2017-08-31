c754e462324bb2ec63a13c21aae56eb0fc8a7b96
billysvk@gmail.com <billysvk@gmail.com>
2017-08-31 23:50:28 +0300

    add new event working!

--- functions/menu.php
+++ functions/menu.php
@@ -1,5 +1,9 @@
 <?php
+$serverName = 'DESKTOP-IQAL01N';
+$connectionInfo=array('Database' => 'calendar');
 
+global $con;
+$con = sqlsrv_connect($serverName, $connectionInfo);
 	function get_menu ()
 	{
 		global $con;
@@ -32,7 +36,7 @@
 		return $row;
 	} // end function get_page
 
-	function get_all_data_for_this_lab($labId, $dateSelected)
+	function get_all_data_for_this_lab($labId, $dateSelected,$temp)
 	{
 		global $con;
 		$row = array ();
@@ -40,8 +44,92 @@
 
 		$sql = "SELECT * FROM event_calendar WHERE labId = $intLabId AND event_date = '$dateSelected'";
 		$result = sqlsrv_query ( $con, $sql);
-	    $row = sqlsrv_fetch_array ( $result );
 
+		while( $rowTemp = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
+         $row[] = $rowTemp;
+        }  
+
+        if(!$row){
+
+				$resultsNULL = "true";
+				$_SESSION['resultsNULLTrue'] = $resultsNULL;
+				//header("Location: ../labs/labpage.php");
+        }else {
+        	    $resultsNULL = "false";
+				$_SESSION['resultsNULLTrue'] = $resultsNULL;
+				//header("Location: ../labs/labpage.php");
+        }
+
+
+        if($temp == "true") {
+			$boxesThatShouldBeClosed = array();
+			 foreach($row as $templab) {
+              //echo "test";
+                 for($i=1; $i<7;$i++){
+                   if ($templab["h_".$i] == 1 ) {
+                     $boxesThatShouldBeClosed[$i-1] = 1;
+                   }
+                 }
+               }
+          if($boxesThatShouldBeClosed){
+          	if( isset($boxesThatShouldBeClosed[0]) ) {
+    			  $h_1 = $boxesThatShouldBeClosed[0];
+			}else{
+ 				  $h_1 = 0;
+			}
+			if( isset($boxesThatShouldBeClosed[1]) ) {
+    			  $h_2 = $boxesThatShouldBeClosed[1];
+			}else{
+ 				  $h_2 = 0;
+			}
+			if( isset($boxesThatShouldBeClosed[2]) ) {
+    			  $h_3 = $boxesThatShouldBeClosed[2];
+			}else{
+ 				  $h_3 = 0;
+			}
+			if( isset($boxesThatShouldBeClosed[3]) ) {
+    			  $h_4 = $boxesThatShouldBeClosed[3];
+			}else{
+ 				  $h_4 = 0;
+			}
+			if( isset($boxesThatShouldBeClosed[4]) ) {
+    			  $h_5 = $boxesThatShouldBeClosed[4];
+			}else{
+ 				  $h_5 = 0;
+			}
+			if( isset($boxesThatShouldBeClosed[5]) ) {
+    			  $h_6 = $boxesThatShouldBeClosed[5];
+			}else{
+ 				  $h_6 = 0;
+			}
+          }
+               return $boxesThatShouldBeClosed;
+        }
+        	
+        
+        if($temp == "false"){
+        	$temparray = array("0","0","0","0","0","0");
+        	$_SESSION['h_1'] = $temparray[0];
+        	$_SESSION['h_2'] = $temparray[1];
+        	$_SESSION['h_3'] = $temparray[2];
+        	$_SESSION['h_4'] = $temparray[3];
+        	$_SESSION['h_5'] = $temparray[4];
+        	$_SESSION['h_6'] = $temparray[5];
+        	return $temparray;
+        }
+        if($temp == "dontCheck") {
+        	  if(!$row){
+
+				$resultsNULL = "true";
+				$_SESSION['resultsNULLTrue'] = $resultsNULL;
+				//header("Location: ../labs/labpage.php");
+        }else {
+        	    $resultsNULL = "false";
+				$_SESSION['resultsNULLTrue'] = $resultsNULL;
+				//header("Location: ../labs/labpage.php");
+        }
+        }
+	    //$row = sqlsrv_fetch_array ( $result );
 		return $row;
 	}
 
@@ -51,8 +139,35 @@
 		$row = array ();
         $intLabId = (int)$data ['labId'];
 
-        $sql = "INSERT INTO event_calendar (labId,event_date,Title,Description,h_1,h_2,h_3,h_4,h_5,h_6,status) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+        $sql = "INSERT INTO event_calendar (labId,event_date,Title,description,h_1,h_2,h_3,h_4,h_5,h_6,status) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
 		$params = array($intLabId, $data ['event_date'],$data ['Title'],$data ['description'], $data ['h_1'],$data ['h_2'],$data ['h_3'],$data ['h_4'],$data ['h_5'],$data ['h_6'],$data ['Status']);
 		$stmt = sqlsrv_query( $con, $sql, $params);
+	}
+
+	function check_if_data_exists($labId, $dateSelected)
+	{
+		global $con;
+		$row = array ();
+        $intLabId = (int)$labId;
+
+		$sql = "SELECT * FROM event_calendar WHERE labId = $intLabId AND event_date = '$dateSelected'";
+		$result = sqlsrv_query ( $con, $sql);
+
+		while( $rowTemp = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
+         $row[] = $rowTemp;
+        }  
+
+        if(!$row){
+
+				$resultsNULL = "true";
+				$_SESSION['resultsNULLTrue'] = $resultsNULL;
+				//header("Location: ../labs/labpage.php");
+        }else {
+        	    $resultsNULL = "false";
+				$_SESSION['resultsNULLTrue'] = $resultsNULL;
+				//header("Location: ../labs/labpage.php");
+        }
+
+		return null;
 	}
 ?>
--- labs/newEventForLab.php
+++ labs/newEventForLab.php
@@ -7,37 +7,37 @@
 				$data ['Title'] =  $_POST ['Title']; // Αν πρόκειται για αλφαριθμητικό, χρησιμοποιούμε την mysql_real_escape_string
 				$data ['description'] = $_POST ['Description']; // Αν θέλουμε ακέραια τιμή, χρησιμοποιούμε την (int)
 				$data ['labId'] =  $_POST ['labId'];
-				if($_POST [1] == null){
+				if(!isset($_POST [1])){
 					$data ['h_1'] = 0;
 				}else{
 					$data ['h_1'] = (int)$_POST [1];
 				}
 
-				if($_POST [2] == null){
+				if(!isset($_POST [2])){
 					$data ['h_2'] = 0;
 				}else{
 					$data ['h_2'] = (int)$_POST [2];
 				}
 
-				if($_POST [3] == null){
+				if(!isset($_POST [3])){
 					$data ['h_3'] = 0;
 				}else{
 					$data ['h_3'] =(int)$_POST [3];
 				}
 
-				if($_POST [4] == null){
+				if(!isset($_POST [4])){
 					$data ['h_4'] = 0;
 				}else{
 					$data ['h_4'] = (int)$_POST [4];
 				}
 
-				if($_POST [5] == null){
+				if(!isset($_POST [5])){
 					$data ['h_5'] = 0;
 				}else{
 					$data ['h_5'] = (int)$_POST [5];
 				}
 
-				if($_POST [6] == null){
+				if(!isset($_POST [6])){
 					$data ['h_6'] = 0;
 				}else{
 					$data ['h_6'] = (int)$_POST [6];
@@ -45,8 +45,9 @@
 				
 				$data ['Status'] =  0;
 
+				
 				add_event_for_this_lab($data);
 
-		header ( "location:labpage.php" ); // Ο χρήστης κατευθύνεται πάλι στη λίστα με τις εγγραφές
+		//header ( "location:labpage.php" ); // Ο χρήστης κατευθύνεται πάλι στη λίστα με τις εγγραφές
 	} // end if
 ?>
